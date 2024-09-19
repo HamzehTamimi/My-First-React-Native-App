fb148d7310b080c6054f8e572870f8fab3020845
HamzehTamimi <63554013+HamzehTamimi@users.noreply.github.com>
2024-09-19 12:26:52 +0300

    Update UserDetails.js

--- screens/UserDetails.js
+++ screens/UserDetails.js
@@ -19,14 +19,14 @@ export default function UserDetails({ route }) {
     <SafeAreaView style={userDetailsStyle.container}>
       <ScrollView>
         <Image style={userDetailsStyle.image} source={{ uri: `https://avatar.iran.liara.run/public/${userId}` }}></Image>
-        <UserDetailsBox label="Name" value={userData.name} />
-        <UserDetailsBox label="Username" value={userData.username} />
-        <UserDetailsBox label="Email" value={userData.email} />
-        <UserDetailsBox label="Street" value={userData.address.street} />
-        <UserDetailsBox label="City" value={userData.address.city} />
-        <UserDetailsBox label="ZIP Code" value={userData.address.zipcode} />
-        <UserDetailsBox label="Phone" value={userData.phone} />
-        <UserDetailsBox label="Website" value={userData.website} />
+        <UserDetailsBox label="Name" value={userData.name}/>
+        <UserDetailsBox label="Username" value={userData.username}/>
+        <UserDetailsBox label="Email" value={userData.email}/>
+        <UserDetailsBox label="Street" value={userData.address?.street}/>
+        <UserDetailsBox label="City" value={userData.address?.city}/>
+        <UserDetailsBox label="ZIP Code" value={userData.address?.zipcode}/>
+        <UserDetailsBox label="Phone" value={userData.phone}/>
+        <UserDetailsBox label="Website" value={userData.website}/>        
       </ScrollView>
     </SafeAreaView>
   );
